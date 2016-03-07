93028e0f9dc5db6afb7a99be6269ae25dd65fc26
Marcberin <mayimark@gmail.com>
2016-03-07 00:37:37 -0800

    linear regression multivariable

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.06;
+num_iters = 600;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,7 +104,12 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+% Display mean and stddev
+fprintf('stddev: %f\n mean: %f \n', sigma, mu);
+room = [1650 3];
+room = (room .- mu) ./ sigma;
+room = [1 room];
+price = room * theta; % You should change this
 
 
 % ============================================================
@@ -135,6 +140,7 @@ X = data(:, 1:2);
 y = data(:, 3);
 m = length(y);
 
+[X mu sigma] = featureNormalize(X);
 % Add intercept term to X
 X = [ones(m, 1) X];
 
@@ -149,7 +155,10 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+room = [1650 3];
+room = (room .- mu) ./ sigma;
+room = [1 room];
+price = room * theta; % You should change this
 
 
 % ============================================================
